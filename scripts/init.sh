4a326ba4b0566dad12377d1d5d1ae3933b462085
lomes0 <lomes0@protonmail.com>
2021-05-23 19:19:01 +0300

    create symbloic links to configs, change tmux theme % add tmux/tmp

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
+# Install dependencies
 sudo apt-get update
-
 sudo apt-get install vim vim-gtk3 cscope tmux git zsh curl
 
+# Install ohmyzsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+# Install fzf
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-
 ~/.fzf/install
 
-# full screen
+# Install Tmux plugin manager
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+# Create symbloic-links to our configs
+# 
+
+
+# TODO open terminal on full screen
 # sudo vim /usr/share/applications/org.gnome.Terminal.desktop 
